8b877eb863e5aacfe14b90c8d0f9e278b2357762
Picoquio <84530694+Picoquio@users.noreply.github.com>
2022-08-31 14:38:17 -0300

    rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'pipesApp';
+
+
+  nombre: string = 'Juan'
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    
+  }
+
+
 }
